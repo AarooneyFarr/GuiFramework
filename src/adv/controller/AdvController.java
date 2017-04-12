662788ecdc5faa71eca8d90a0cfe9bba0613e8e0
AarooneyFarr <Aarooney.Farr@gmail.com>
2017-04-12 08:08:34 -0600

    added cowboy

--- src/adv/controller/AdvController.java
+++ src/adv/controller/AdvController.java
@@ -1,11 +1,14 @@
 package adv.controller;
 
 import adv.view.*;
+import adv.model.*;
+import java.util.ArrayList;
 
 public class AdvController
 	{
 
 		private AdvFrame appFrame;
+		private ArrayList<CowboyPizza> myPizzas;
 		
 		public AdvController()
 		{
@@ -17,4 +20,9 @@ public class AdvController
 			
 		}
 		
+		private void setupList()
+		{
+			myPizzas.add(new CowboyPizza());
+			
+		}
 	}
---
+++ src/adv/model/Pizzable.java
@@ -0,0 +1,22 @@
+package adv.model;
+
+/**
+ * 
+ * @author AaronFarr
+ * @author MattPowley
+ * @author KadinHatchell
+ * @author AmmarNazir
+ * @author JackMachara
+ *
+ */
+public interface Pizzable
+	{
+		
+		public boolean hasPineapple();
+		
+		public int getPepperoni();
+		public void setPepperoni(int pepperoni);
+		
+		public void toss();
+
+	}
