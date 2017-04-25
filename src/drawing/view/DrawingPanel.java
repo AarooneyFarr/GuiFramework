0b2639d692a88107924eb45682a47ef2660f4482
AarooneyFarr <Aarooney.Farr@gmail.com>
2017-04-25 09:56:26 -0600

    add

--- src/drawing/view/DrawingPanel.java
+++ src/drawing/view/DrawingPanel.java
@@ -16,6 +16,10 @@ public class DrawingPanel extends JPanel
 	{
 		private ShapePanel shapePanel;
 		private JButton rectangleButton;
+		private JButton triangleButton;
+		private JButton circleButton;
+		private JButton ellipseButton;
+		private JButton polygonButton;
 		private DrawingController baseController;
 
 		public DrawingPanel(DrawingController baseController)
@@ -25,6 +29,10 @@ public class DrawingPanel extends JPanel
 
 				shapePanel = new ShapePanel(baseController);
 				rectangleButton = new JButton("Draw Rectangles");
+				triangleButton = new JButton("Draw Triangles");
+				circleButton = new JButton("Draw Circles");
+				polygonButton = new JButton("Draw Polygons");
+				ellipseButton = new JButton("Draw Ovals");
 				
 				
 				setupPanel();
@@ -37,12 +45,26 @@ public class DrawingPanel extends JPanel
 				this.setLayout(null);
 				this.setBackground(Color.GRAY);
 				this.add(rectangleButton);
+				this.add(triangleButton);
+				this.add(circleButton);
+				this.add(ellipseButton);
+				this.add(polygonButton);
 				this.add(shapePanel);
 			}
 
 		public void setupLayout()
 			{
-				rectangleButton.setBounds(180,391,148,54);
+				rectangleButton.setBounds(176,391,148,54);
+				triangleButton.setBounds(6, 391, 148, 54);
+				circleButton.setBounds(346, 391, 148, 54);
+				polygonButton.setBounds(285, 325, 148, 54);
+				ellipseButton.setBounds(69, 325, 148, 54);
+
+
+
+
+				
+				
 				shapePanel.setBounds(0,0,500,250);
 			}
 
@@ -54,7 +76,43 @@ public class DrawingPanel extends JPanel
 						public void actionPerformed(ActionEvent e)
 							{
 								shapePanel.addRectangles();
-								shapePanel.setupShapes();
+								
+							}
+					});
+				triangleButton.addActionListener(new ActionListener()
+					{
+
+						public void actionPerformed(ActionEvent e)
+							{
+								shapePanel.addTriangles();
+								
+							}
+					});
+				circleButton.addActionListener(new ActionListener()
+					{
+
+						public void actionPerformed(ActionEvent e)
+							{
+								shapePanel.addCircles();
+								
+							}
+					});
+				ellipseButton.addActionListener(new ActionListener()
+					{
+
+						public void actionPerformed(ActionEvent e)
+							{
+								shapePanel.addEllipses();
+								
+							}
+					});
+				polygonButton.addActionListener(new ActionListener()
+					{
+
+						public void actionPerformed(ActionEvent e)
+							{
+								shapePanel.addPolygons();
+								
 							}
 					});
 			}
